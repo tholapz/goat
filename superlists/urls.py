99200249bdf7d5f24bb5e5a285c07c9a0b54641e
tholapz <tholapz@gmail.com>
2014-03-15 23:08:02 -0700

    first commit

---
+++ superlists/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^lists/', include('lists.urls')),
+    # url(r'^admin/', include(admin.site.urls)),
+)
